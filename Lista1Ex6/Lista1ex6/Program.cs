5dcca7b2ee24e00c6f3541e59a9dcb9a5d090d60
nini07770 <altino.nicole@aluno.ifsp.edu.br>
2025-04-18 17:59:53 -0300

    Add files via upload

---
+++ Lista1Ex6/Lista1ex6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor1, valor2;
+            double mediageo;
+
+            Console.Write("Digite o primeiro número: ");
+            valor1 = Double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo número: ");
+            valor2 = Double.Parse(Console.ReadLine());
+
+            mediageo = Math.Sqrt(valor1 * valor2);
+
+            Console.WriteLine("A média geométrica dos números {0} e {1} é: {2}", valor1, valor2, mediageo);
+           
+            
+        }
+    }
+}
