1f99441e6b40f8e42d83abb3fddc0558bc51ba8a
nini07770 <altino.nicole@aluno.ifsp.edu.br>
2025-04-18 15:59:40 -0300

    Add files via upload

---
+++ Lista1ex4/Lista1ex4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double baze;
+            double altura;
+            double area;
+
+            Console.Write("Digite a base do triângulo: ");
+            baze = Double.Parse(Console.ReadLine());
+
+            Console.Write("Digite a altura do triângulo: ");
+            altura = Double.Parse(Console.ReadLine());
+
+            area = (baze * altura) / 2;
+
+            Console.WriteLine("A área do triângulo de Base {0} e altura {1} é: {2}", baze, altura, area);
+
+        }
+    }
+}
