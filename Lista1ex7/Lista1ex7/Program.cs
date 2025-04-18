77039ed982bbff448367856938e0023f719a2093
nini07770 <altino.nicole@aluno.ifsp.edu.br>
2025-04-18 17:10:57 -0300

    Add files via upload

---
+++ Lista1ex7/Lista1ex7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double milhas;
+            double km;
+
+            Console.WriteLine("Digite a distância em milhas: ");
+            milhas = Double.Parse(Console.ReadLine());
+
+            km = milhas * 1.852;
+            Console.WriteLine("A distância de {0} milhas em quilômetros é: {1} km", milhas, km);
+           
+        }
+    }
+}
