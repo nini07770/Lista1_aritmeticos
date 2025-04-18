814fdf430ccb7c9719114dcd25880a0fd97686a6
nini07770 <altino.nicole@aluno.ifsp.edu.br>
2025-04-18 17:24:04 -0300

    Add files via upload

---
+++ Lista1ex8/Lista1ex8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1ex8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double grausC;
+            double grausF;
+
+            Console.Write("Digite a temperatura em graus Celsius: ");
+            grausC = Double.Parse(Console.ReadLine());
+
+            grausF = (grausC * 9 / 5) + 32;
+
+            Console.WriteLine("A temperatura {0}ºC em graus Fahrenheit é: {1}ºF", grausC, grausF);
+
+        }
+    }
+}
