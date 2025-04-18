ae352cc8afd053203a4e83b4cdce3d2bb9f561aa
nini07770 <altino.nicole@aluno.ifsp.edu.br>
2025-04-18 18:18:41 -0300

    Add files via upload

---
+++ Lista1ex9/Lista1ex9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1ex9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double diametro;
+            double area;
+
+            Console.Write("Digite o diâmetro do círculo: ");
+            diametro = Double.Parse(Console.ReadLine());
+
+            area = 3.14 * (diametro / 2) * (diametro / 2);
+
+            Console.WriteLine("A área do círculo com diâmetro {0} é: {1}", diametro, area);
+        }
+    }
+}
