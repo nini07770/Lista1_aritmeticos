42479580aaee7d456421fbd056775b8492ebf699
nini07770 <altino.nicole@aluno.ifsp.edu.br>
2025-04-18 18:32:41 -0300

    Add files via upload

---
+++ Lista1ex10/Lista1ex10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1ex10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double cotacaoDolar;
+            double valorEmDolar;
+            double valorEmReal;
+
+            Console.Write("Digite a cotação do dólar atual: ");
+            cotacaoDolar = Double.Parse(Console.ReadLine());
+
+            Console.Write("Digite um valor em dólar: ");
+            valorEmDolar = Double.Parse(Console.ReadLine());
+
+            valorEmReal = cotacaoDolar * valorEmDolar;
+
+            Console.WriteLine("O valor de {0} dólares é equivalente a {1} reais", valorEmDolar, valorEmReal);
+
+        }
+    }
+}
