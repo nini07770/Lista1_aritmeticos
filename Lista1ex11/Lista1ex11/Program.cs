a18a6d566258adbbcd7a224a526bd022c9807d16
nini07770 <altino.nicole@aluno.ifsp.edu.br>
2025-04-18 18:52:16 -0300

    Add files via upload

---
+++ Lista1ex11/Lista1ex11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1ex11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double X;
+            double Y;
+            double resultado;
+
+            Console.Write("Digite o valor de X: ");
+            X = Double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor de Y: ");
+            Y = Double.Parse(Console.ReadLine());
+
+            resultado = Math.Pow(X, Y);
+
+            Console.WriteLine("O resultado de {0} elevado a {1} é: {2}", X, Y, resultado);
+
+        }
+    }
+}
